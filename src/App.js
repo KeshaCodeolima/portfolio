875b4aed78865387cc161ee82ad21d0255ea7664
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-09-11 13:04:42 +0530

    Add the about page to App.js file to do the navigation.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Navigation from './Components/Navigation/navigation';
 import Home from './Components/Home/home';
+import About from './Components/About/about';
 
 function App() {
   return (
     <>
       <Navigation />
+
       <section id="home">
         <Home />
+      </section>
+
+      <section id='about'>
+        <About />
       </section>
     </>
   );
