5711e59e24b173fb2ff49b54520c8714c1315752
KeshanKulasekara <159102480+KeshanKulasekara@users.noreply.github.com>
2026-09-11 12:19:59 +0530

    Add the home page to App.js file to do the navigation.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navigation from './Components/Navigation/navigation';
+import Home from './Components/Home/home';
 
 function App() {
   return (
     <>
-      <Navigation/>
+      <Navigation />
+      <section id="home">
+        <Home />
+      </section>
     </>
   );
 }
